d4923d17d41ddabdf8c66b3f66dec7e1faebc6ec
Ivan <ivan@vidata.io>
2021-11-14 21:03:56 +0100

    adding Tone.js and played sounds on game buttons

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import * as Tone from 'tone'
+
 var $infoModal = document.getElementById('info-modal');
 var $infoButton = document.getElementById('info-button');
 var $infoClose = document.getElementsByClassName('close')[0];
@@ -21,10 +23,33 @@ $simoneButtons.forEach(function($button) {
   $button.addEventListener('click', function (target) {
     $button.classList.toggle('click');
 
-    if ($button.classList.contains('red')) $button.innerHTML = 'Do';
-    if ($button.classList.contains('blue')) $button.innerHTML = 'Ré';
-    if ($button.classList.contains('green')) $button.innerHTML = 'Mi';
-    if ($button.classList.contains('yellow')) $button.innerHTML = 'Fa';
+    const synth = new Tone. Synth().toDestination();
+    const now = Tone.now();
+    const delay = 0.4;
+    
+    if ($button.classList.contains('red')) {
+      $button.innerHTML = 'Do';
+      synth.triggerAttack("C4", now)
+      synth.triggerRelease(now + delay)
+    }
+    
+    if ($button.classList.contains('blue')) {
+      $button.innerHTML = 'Ré';
+      synth.triggerAttack("D4", now)
+      synth.triggerRelease(now + delay)
+    }
+    
+    if ($button.classList.contains('green')) {
+      $button.innerHTML = 'Mi';
+      synth.triggerAttack("E4", now)
+      synth.triggerRelease(now + delay)
+    }
+    
+    if ($button.classList.contains('yellow')) {
+      $button.innerHTML = 'Fa';
+      synth.triggerAttack("F4", now)
+      synth.triggerRelease(now + delay)
+    }
 
     setTimeout(() => {
       $button.classList.toggle('click');

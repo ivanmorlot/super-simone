f65fcc3e7c7a6631ff1253496eb77f0d5df3aa4b
Ivan <ivan@vidata.io>
2021-11-15 01:44:18 +0100

    adding bot and game over logic

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import Game from './game';
 
-var $infoModal = document.getElementById('info-modal');
-var $infoButton = document.getElementById('info-button');
-var $infoClose = document.getElementsByClassName('close')[0];
+const game = new Game();
+game.init();
+
+var $infoModal = document.querySelector('#info-modal');
+var $infoButton = document.querySelector('#info-button');
+var $infoClose = document.querySelector('#info-modal .close');
+var $gameOverModal = document.querySelector('#game-over-modal');
+var $gameOverClose = document.querySelector('#game-over-modal .close');
+var $gameOverNewGame = document.querySelector('#game-over-modal .game-start');
 
 $infoButton.onclick = function() {
   $infoModal.style.display = 'block';
@@ -12,11 +18,20 @@ $infoClose.onclick = function() {
   $infoModal.style.display = 'none';
 }
 
+$gameOverClose.onclick = function() {
+  $gameOverModal.style.display = 'none';
+}
+
+$gameOverNewGame.onclick = function() {
+  $gameOverModal.style.display = 'none';
+}
+
 window.onclick = function(event) {
   if (event.target == $infoModal) {
     $infoModal.style.display = 'none';
   }
-}
 
-const game = new Game();
-game.init();
+  if (event.target == $gameOverModal) {
+    $gameOverModal.style.display = 'none';
+  }
+}
--- src/game.js
+++ src/game.js
@@ -3,10 +3,11 @@ import * as Tone from 'tone';
 export default class Game {
   constructor(gameConsoleSelector = ".game-console") {
     this.$gameConsole = document.querySelector(gameConsoleSelector);
-    this.newGame();
+
+    this.setup();
   }
 
-  newGame() {
+  setup() {
     this.isGameActive = false;
 
     this.actionDuration = 2000;
@@ -21,8 +22,15 @@ export default class Game {
     this.$gameConsole.querySelector('.game-actions').innerHTML = '0';
 
     this.isPlayerTurn = false;
-    this.$gameConsole.querySelector('.game-turn span').style.fontWeight = 'normal';
     this.$gameConsole.querySelector('.game-turn span').innerHTML = '';
+  }
+
+  newGame() {
+    this.setup();
+
+    this.isGameActive = true;
+
+    this.startRounds();
   }
 
   init() {
@@ -30,20 +38,19 @@ export default class Game {
 
     document.querySelectorAll('.game-start').forEach(function($button) {
       $button.addEventListener('click', function () {
-        gameThis.isGameActive = true;
-        gameThis.startRounds();
+        gameThis.newGame();
       });
     });
 
     document.querySelectorAll('.game-reset').forEach(function($button) {
       $button.addEventListener('click', function () {
-        gameThis.newGame();
+        gameThis.setup();
       });
     });
 
     this.$gameConsole.querySelectorAll('.game-button').forEach(function($button) {
       $button.addEventListener('click', function () {
-        if (gameThis.isPlayerTurn) {
+        if (gameThis.isPlayerTurn && gameThis.isGameActive) {
           const synth = new Tone.Synth().toDestination();
           const now = Tone.now();
           const delay = 0.4;
@@ -91,112 +98,161 @@ export default class Game {
     });
   }
 
-  eventify = function(arr, callback) {
-    arr.push = function(e) {
-        Array.prototype.push.call(arr, e);
-        callback(arr);
-    };
-  };
-
   randomNote() {
     return Math.floor(Math.random() * 4);
   }
 
   botTurn() {
-    const gameThis = this;
+    if (this.isGameActive) {
+      const gameThis = this;
 
-    this.isPlayerTurn = false;
-    this.$gameConsole.querySelector('.game-turn span').style.fontWeight = 'normal';
-    this.$gameConsole.querySelector('.game-turn span').innerHTML = "C'est au tour de Simone.";
+      const $redButton = this.$gameConsole.querySelector('.game-button.red');
+      const $blueButton = this.$gameConsole.querySelector('.game-button.blue');
+      const $greenButton = this.$gameConsole.querySelector('.game-button.green');
+      const $yellowButton = this.$gameConsole.querySelector('.game-button.yellow');
 
-    const $redButton = this.$gameConsole.querySelector('.game-button.red');
-    const $blueButton = this.$gameConsole.querySelector('.game-button.blue');
-    const $greenButton = this.$gameConsole.querySelector('.game-button.green');
-    const $yellowButton = this.$gameConsole.querySelector('.game-button.yellow');
+      this.botActions.push(this.randomNote());
 
-    this.botActions.push(this.randomNote());
+      if (this.validatedRounds < 8) {
+        this.actionDuration = this.actionDuration - 150;
+      }
 
-    if (this.validatedRounds < 8) {
-      this.actionDuration = this.actionDuration - 150;
-    }
+      this.botActions.forEach(function(action, index) {
+        setTimeout(function () {
+          const synth = new Tone.Synth().toDestination();
+          const now = Tone.now();
+          const delay = 0.4;
 
-    this.botActions.forEach(function(action, index) {
-      setTimeout(function () {
-        const synth = new Tone.Synth().toDestination();
-        const now = Tone.now();
-        const delay = 0.4;
-
-        if (action === 0) {
-          $redButton.classList.toggle('click');
-
-          $redButton.innerHTML = 'Do';
-          synth.triggerAttack("C4", now);
-          synth.triggerRelease(now + delay);
-
-          setTimeout(() => {
+          if (action === 0) {
             $redButton.classList.toggle('click');
-            $redButton.innerHTML = '';
-          }, 500);
-        }
-        
-        if (action === 1) {
-          $blueButton.classList.toggle('click');
 
-          $blueButton.innerHTML = 'Ré';
-          synth.triggerAttack("D4", now);
-          synth.triggerRelease(now + delay);
+            $redButton.innerHTML = 'Do';
+            synth.triggerAttack("C4", now);
+            synth.triggerRelease(now + delay);
 
-          setTimeout(() => {
+            setTimeout(() => {
+              $redButton.classList.toggle('click');
+              $redButton.innerHTML = '';
+            }, 500);
+          }
+          
+          if (action === 1) {
             $blueButton.classList.toggle('click');
-            $blueButton.innerHTML = '';
-          }, 500);
-        }
-        
-        if (action === 2) {
-          $greenButton.classList.toggle('click');
 
-          $greenButton.innerHTML = 'Mi';
-          synth.triggerAttack("E4", now);
-          synth.triggerRelease(now + delay);
+            $blueButton.innerHTML = 'Ré';
+            synth.triggerAttack("D4", now);
+            synth.triggerRelease(now + delay);
 
-          setTimeout(() => {
+            setTimeout(() => {
+              $blueButton.classList.toggle('click');
+              $blueButton.innerHTML = '';
+            }, 500);
+          }
+          
+          if (action === 2) {
             $greenButton.classList.toggle('click');
-            $greenButton.innerHTML = '';
-          }, 500);
-        }
-        
-        if (action === 3) {
-          $yellowButton.classList.toggle('click');
 
-          $yellowButton.innerHTML = 'Fa';
-          synth.triggerAttack("F4", now);
-          synth.triggerRelease(now + delay);
+            $greenButton.innerHTML = 'Mi';
+            synth.triggerAttack("E4", now);
+            synth.triggerRelease(now + delay);
 
-          setTimeout(() => {
+            setTimeout(() => {
+              $greenButton.classList.toggle('click');
+              $greenButton.innerHTML = '';
+            }, 500);
+          }
+          
+          if (action === 3) {
             $yellowButton.classList.toggle('click');
-            $yellowButton.innerHTML = '';
-          }, 500);
-        }
-      }, index * gameThis.actionDuration);
-    });
+
+            $yellowButton.innerHTML = 'Fa';
+            synth.triggerAttack("F4", now);
+            synth.triggerRelease(now + delay);
+
+            setTimeout(() => {
+              $yellowButton.classList.toggle('click');
+              $yellowButton.innerHTML = '';
+            }, 500);
+          }
+        }, index * gameThis.actionDuration);
+      });
+    }
+  }
+
+  setPlayerTurn(isPlayerTurn) {
+    const $turn = this.$gameConsole.querySelector('.game-turn span');
+
+    if (isPlayerTurn) {
+      this.isPlayerTurn = true;
+      this.$gameConsole.querySelectorAll('.game-button').forEach(function ($button) {
+        $button.classList.add('player');
+      });
+      $turn.style.color = 'rgb(155 233 33)';
+      $turn.innerHTML = "C'est à vous !";
+    } else {
+      this.isPlayerTurn = false;
+      this.$gameConsole.querySelectorAll('.game-button').forEach(function ($button) {
+        $button.classList.remove('player');
+      });
+      $turn.style.color = 'rgb(255 169 13)';
+      $turn.innerHTML = "C'est au tour de Simone.";
+    }
   }
 
   playerTurn() {
-    this.isPlayerTurn = true;
-    this.$gameConsole.querySelector('.game-turn span').style.fontWeight = 'bold';
-    this.$gameConsole.querySelector('.game-turn span').innerHTML = "C'est à vous !";
+    if (this.isGameActive) {
+      this.setPlayerTurn(true);
+    }
   }
 
   newRound() {
-    this.botTurn();
-    setTimeout(() => this.playerTurn(), this.botActions.length * this.actionDuration );
+    if (this.isGameActive) {
+      const gameThis = this;
+
+      const eventify = function(arr, callback) {
+        arr.push = function(e) {
+            Array.prototype.push.call(arr, e);
+            callback(arr);
+        };
+      };
+
+      eventify(this.playerActions, function(playerActions) {
+        const equals = (a, b) => a.length === b.length && a.every((v, i) => v === b[i]);
+
+        if (gameThis.botActions.length >= playerActions.length) {
+          const relevantBotActions = gameThis.botActions.slice(0, playerActions.length);
+
+          if (equals(relevantBotActions, playerActions)) {
+            gameThis.validatedActions++;
+            gameThis.$gameConsole.querySelector('.game-actions').innerHTML = gameThis.validatedActions;
+          } else {
+            gameThis.setup()
+            document.querySelector('#game-over-modal').style.display = 'block';
+          }
+        }
+
+        if (gameThis.botActions.length === playerActions.length && equals(gameThis.botActions, playerActions)) {
+          gameThis.validatedRounds++;
+          gameThis.$gameConsole.querySelector('.game-rounds').innerHTML = gameThis.validatedRounds;
+
+          gameThis.playerActions = [];
+
+          gameThis.setPlayerTurn(false);
+
+          setTimeout(() => gameThis.newRound(), 2000);
+        }
+      });
+
+      this.botTurn();
+
+      setTimeout(() => this.playerTurn(), this.botActions.length * this.actionDuration );
+    }
   }
 
   startRounds() {
-    this.eventify(this.playerActions, function(playerActions) {
-      // TODO: when player click a button, check arrays are equal when they have same length
-    });
-
-    this.newRound();
+    if (this.isGameActive) {
+      this.setPlayerTurn(false);
+      setTimeout(() => this.newRound(), 1200);
+    }
   }
 }
